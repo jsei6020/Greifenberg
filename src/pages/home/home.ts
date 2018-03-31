feed3d4edd589911257e049b53553c999c786fd9
jsei6020 <jakob.seidler@gmx.net>
2018-03-31 12:08:53 +0200

    Set Up

---
+++ src/pages/home/home.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+	public ort: string;
+	public feed: Array<string>;
+	public options: Array<string>;
+
+  constructor(public navCtrl: NavController) {
+  	this.ort = "Stadttor";
+  	this.feed = ["Du stehst vor einem großen Stadttor.", "Jemand kommt auf dich zu."];
+  	this.options = ["Hallo sagen", "Weiter gehen", "Anrempeln", "Umarmung probieren"];
+  }
+
+}
