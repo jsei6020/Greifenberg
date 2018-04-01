5fa7b913c041a153b0c59dec7e00f5f99f292164
Fabian Cap <fabiancap@Fabians-MacBook-Pro.local>
2018-04-01 11:56:30 +0200

    Charaktererstellunspage

---
+++ src/pages/charakter-erstellung/charakter-erstellung.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { HomePage } from '../home/home';
+/**
+ * Generated class for the CharakterErstellungPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-charakter-erstellung',
+  templateUrl: 'charakter-erstellung.html',
+})
+export class CharakterErstellungPage {
+
+  hello;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.hello = "Hello";
+  }
+
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad CharakterErstellungPage');
+
+  }
+
+waehlen(Typ) {
+this.navCtrl.push(HomePage, {
+      Typ: Typ
+    });
+}
+
+}
